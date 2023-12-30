f05842c65daa5334014962ebb7b8c1baa108dfd0
rajveer43 <rajveer.rathod1301@gmail.com>
2023-12-30 21:07:16 +0530

    Add Favorite Papers Section to Profile Dashboard (generated by blackbox.ai )

--- src/pages/ProfileDashboard/ProfileDashboard.tsx
+++ src/pages/ProfileDashboard/ProfileDashboard.tsx
@@ -4,6 +4,7 @@ import UserProfileCard from '../../components/UserProfile/UserProfileCard';
 import MyGraphsSection from '../../components/UserProfile/MyGraphsSection';
 import FavoritePapersSection from '../../components/UserProfile/FavoritePapersSection';
 import './ProfileDashboard.css';
+
 const ProfileDashboard: React.FC = () => {
   // Mock data, replace with actual data from your application or API
   const userData = {
@@ -22,19 +23,23 @@ const ProfileDashboard: React.FC = () => {
 
   return (
     <div className="profile-dashboard-container container mx-auto p-4">
+      {/* User Profile Card Section */}
       <div className="user-profile-card-container bg-white rounded-md shadow-md p-6 mb-6">
         <UserProfileCard {...userData} />
       </div>
 
+      {/* Recently Made Graphs Section */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="recent-graphs-container bg-white rounded-md shadow-md p-6">
           <h2 className="text-xl font-semibold mb-4">Recently Made Graphs</h2>
           <MyGraphsSection recentlyMadeGraphs={recentlyMadeGraphs} />
         </div>
-        <div className="favorite-papers-container bg-white rounded-md shadow-md p-6">
-          <h2 className="text-xl font-semibold mb-4">Favorite Papers</h2>
-          <FavoritePapersSection favoritePapers={favoritePapers} paperCollections={paperCollections} />
-        </div>
+      </div>
+
+      {/* Favorite Papers Section */}
+      <div className="favorite-papers-container bg-white rounded-md shadow-md p-6">
+        <h2 className="text-xl font-semibold mb-4">Favorite Papers</h2>
+        <FavoritePapersSection favoritePapers={favoritePapers} paperCollections={paperCollections} />
       </div>
     </div>
   );
